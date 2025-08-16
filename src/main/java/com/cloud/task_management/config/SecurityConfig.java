3063f03c97a600e298f7ea109a96004a8654ec9b
abhinavmarth <abhinavmartha555@gmail.com>
2025-08-17 01:16:01 +0530

    Changed Files

--- src/main/java/com/cloud/task_management/config/SecurityConfig.java
+++ src/main/java/com/cloud/task_management/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors->cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
